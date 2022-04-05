88e720ffb422f90836ab2ef5c4e40e90f683b5ed
Eana Hufwe <eana@1a23.com>
2022-04-04 20:39:04 -0700

    Vocaloid ❤️ U

--- sekai-singIt/README.md
+++ sekai-singIt/README.md
@@ -3,8 +3,28 @@ Well, it’s just too usual to hide a flag in stegano, database, cipher, or serv
 
 ## Writeup
 
-The source code suggest that the music file was generated based on the `flag.txt`, and choosing MP3 files from the downloaded JSON.
+The source code suggest that the music file was generated based on the content of `flag.txt`, and choosing MP3 files from the downloaded JSON.
 
 Thus, download the JSON and all MP3 files, then match each second of it against files to see which songs are included, then pick out the first character of each song’s name as the flag.
 
-`SEKaエ「ボオカロいドウタおう＊`
+Players can either match the tracks manually if they want to, or it is also possible to use tools like [dejavu] for automation.
+
+[dejavu]: https://github.com/worldveil/dejavu
+
+`SEKAI{v0caloId<3u}`
+
+> Vocaloid ❤️ U
+
+## Credits
+
+* [＊ハロー、プラネット。 / sasakure.UK](https://youtu.be/1gHHgx8bTxc)
+* [ワールドイズマイン / ryo (supercell)](http://www.nicovideo.jp/watch/sm3504435)
+* [自傷無色 / ねこぼーろ（ササノマリイ）](http://www.nicovideo.jp/watch/sm19870840)
+* [霽れを待つ / Orangestar](https://youtu.be/wvlUWjqGQSA)
+* [愛されなくても君がいる / ピノキオピー](https://youtu.be/ygY2qObZv24)
+* [カトラリー / 有機酸](https://youtu.be/HHhFX9zUV2s)
+* [ニア / 夏代孝明](http://www.nicovideo.jp/watch/sm31477166)
+* [ECHO / Crusher](https://youtu.be/cQKGUgOfD8U)
+* [悔やむと書いてミライ / まふまふ](https://youtu.be/jUyCN1229Ws)
+* [セカイはまだ始まってすらいない / ピノキオピー](https://youtu.be/1s8NNPgdl5g)
+* [ODDS & ENDS / ryo (supercell)](https://youtu.be/6OmwKZ9r07o)
--- sekai-singIt/sing_it.py
+++ sekai-singIt/sing_it.py
@@ -5,16 +5,12 @@ import subprocess
 tracks = requests.get("https://sekai-world.github.io/sekai-master-db-diff/musics.json").json()
 resources = requests.get("https://sekai-world.github.io/sekai-master-db-diff/musicVocals.json").json()
 
-def pick(char):
-    return random.choice([i["id"] for i in tracks if i["title"].startswith(char)])
-
 def get_resource(mid):
-    return sorted([i for i in resources if i["musicId"] == mid], key=lambda a: a["caption"])[-1]["assetbundleName"]
+    return random.choice([i for i in resources if i["musicId"] == mid], key=lambda a: a["caption"])["assetbundleName"]
 
 def download(mid):
     resource = get_resource(mid)
     r = requests.get(f"https://sekai-res.dnaroma.eu/file/sekai-assets/music/short/{resource}_rip/{resource}_short.mp3")
-    print(resource)
     filename = f"tracks/{mid}.mp3"
     with open(filename, "wb") as f:
         f.write(r.content)
@@ -23,10 +19,12 @@ def download(mid):
 with open("flag.txt") as f:
     flag = f.read().strip()
 
-tracks = [download(pick(i)) for i in flag]
+assert flag.startswith("SEKAI{") and flag.endswith("}")
+flag = flag[6:-1]
+tracks = [download(ord(i)) for i in flag]
 
 inputs = sum([["-i", f"tracks/{i}.mp3"] for i in tracks], [])
-filters = "".join(f"[{i}:a]atrim=end=1,asetpts=PTS-STARTPTS[a{i}];" for i in range(len(tracks))) + \
+filters = "".join(f"[{i}:a]atrim=end=3,asetpts=PTS-STARTPTS[a{i}];" for i in range(len(tracks))) + \
           "".join(f"[a{i}]" for i in range(len(tracks))) + \
           f"concat=n={len(tracks)}:v=0:a=1[a]"
 
