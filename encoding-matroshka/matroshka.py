8c8c3128e60918356edb73f949c5bd78383a5978
Eana Hufwe <eana@1a23.com>
2022-09-30 21:00:31 -0700

    Matroshka lite 2 update

--- encoding-matroshka/matroshka.py
+++ encoding-matroshka/matroshka.py
@@ -5,9 +5,11 @@ p = ""
 for i in range(0, len(d), 8):
     l = d[i:i+4]
     h = d[i+4:i+8]
-    he = 30 if h[0] == "0" else 90
+    he = 40 if h[0] == "0" else 100
     he += int(h[1:], 2)
     le = 40 if l[0] == "0" else 100
     le += int(l[1:], 2)
-    p += f"\033[{he};{le}m☻\033[0m"
+    p += f"\033[{he}m☻\033[0m"
+    p += f"\033[{le}m☻\033[0m"
+    print(hex(int(l, 2))[2:], hex(int(h, 2))[2:])
 print(p)
